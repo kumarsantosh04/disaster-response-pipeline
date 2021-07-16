8b9481ac0feee209fa47d9954f89270518a6f680
santosh kumar <santoshkumar1t3@gmail.com>
2021-07-16 18:04:49 +0530

    UPDATE: update heroku

--- app/run.py
+++ app/run.py
@@ -168,7 +168,8 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 3001))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 if __name__ == '__main__':
